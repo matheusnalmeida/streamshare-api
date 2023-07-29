ab6d35d6153f997a2675e646478033c048f07f8f
matheusnalmeida <matheus.nunes00@souunit.com.br>
2023-07-29 12:35:06 -0300

    Create initial tests integration structure

---
+++ tests/StreamShare.Test.Integration/Fixtures/GerenciaNetFixture.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace StreamShare.Test.Integration.Fixtures
+{
+    public class GerenciaNetFixture : IDisposable
+    {
+        public JObject Credentials { get; private set; }
+        public string QRCodeRelativePath { get; private set; }
+
+        public GerenciaNetFixture()
+        {
+            JObject configurationJson = JObject.Parse(File.ReadAllText(@"Credentials\gerencianet.configuration.json"));
+            Credentials = (JObject)configurationJson["credentials"];
+            QRCodeRelativePath = (string)configurationJson["qr_code_relative_path"];
+        }
+
+        public void Dispose()
+        {
+            // ... clean up test data
+            Credentials = null;
+            QRCodeRelativePath = null;        
+        }
+    }
+}
